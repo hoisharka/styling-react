fd7e8ccb4a36e05e0d93e8193c3545847896d8d8
hoisharka <hoisharka@gmail.com>
2017-12-28 00:42:07 +0900

    sass-button

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
 import React, { Component } from 'react';
-import classNames from 'classnames/bind';
-import styles from './App.scss';
-console.log(styles);
-const cx = classNames.bind(styles);
+import Button from './components/Button';
 
 class App extends Component {
   render() {
-    const isBlue = true;
-
     return (
-      <div className={cx('box', {
-        blue: isBlue
-      })}>
-        <div className={cx('box-inside')}/>
+      <div>
+        <Button>버튼</Button>
       </div>
-    );
+    )
   }
 }
 
