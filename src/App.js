f355df346fcd51eb3f369a86cfb8736c1ad71e6b
hoisharka <hoisharka@gmail.com>
2017-12-27 20:10:47 +0900

    sass

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
-import classNames from 'classnames';
-import styles from './App.css';
+import classNames from 'classnames/bind';
+import styles from './App.scss';
+console.log(styles);
 const cx = classNames.bind(styles);
 
 class App extends Component {
   render() {
+    const isBlue = true;
+
     return (
       <div className={cx('box', 'blue')}>
-
+        <div className={cx('box-inside')}/>
       </div>
     );
   }
