846836035a1713cf892f716943b2bb8d1cf5c4dc
hoisharka <hoisharka@gmail.com>
2017-12-27 18:29:17 +0900

    CSSModule

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import classNames from 'classnames';
+import styles from './App.css';
+const cx = classNames.bind(styles);
+
+class App extends Component {
+  render() {
+    return (
+      <div className={cx('box', 'blue')}>
+
+      </div>
+    );
+  }
+}
+
+export default App;
